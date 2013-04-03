fbe635ebec29551e289b70c0140674e5acc2d382
mitchjanrain <mitch@janrain.com>
2013-04-03 15:13:48 -0700

    Fixed a bug to test Jenkins

--- src/com/example/helloandroid/MainActivity.java
+++ src/com/example/helloandroid/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        DEADBEEF; // DEADBEEF is bad code. Comment out to fix this.
+        // DEADBEEF; // DEADBEEF is bad code. Comment out to fix this.
     }
 
     @Override
